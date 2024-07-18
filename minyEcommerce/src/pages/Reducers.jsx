075145bdc943c02052c9a989cae2ef08e2e83aed
aryanPatel <aryanpatel1248@gmail.com>
2024-07-18 22:36:08 +0530

    practice use reducer

--- minyEcommerce/src/pages/Reducers.jsx
+++ minyEcommerce/src/pages/Reducers.jsx
@@ -1,53 +1,97 @@
-// import { useReducer } from "react"
-// function reducer(state,action){
-//    if(action.type==='incrementage'){
-//     return{
-//         age:state.age+1
-//    };
-//    }
+//  import { useReducer } from "react";
+
+// function dispatchFunc(state,action){
+//      switch(action.type){
+//         case 'increment':{
+//             return{
+//                 value:state.value+10
+//             };
+//         }
+//         case 'decrement':{
+//             return{
+//                 value:state.value-10
+//             };
+//         }
+//         default :{
+//             return state;
+//         }
+//      }
 // }
 // export default function Reducers(){
-//     const [state,dispatch]=useReducer(reducer,{age:11});
-
-    
+//     const [state,dispatch]=useReducer(dispatchFunc,{value:100});
 //     return(
 //         <>
-//         <h1>reducer function</h1>
-//         <button onClick={()=>dispatch({type:'incrementage'})}>increment age</button>
-//         <h4>hello you are :{state.age}</h4>
+//         <h3>{state.value}</h3>
+//        <button onClick={()=>dispatch({type:'increment'})}>increment by 10 </button>
+//        <button onClick={()=>dispatch({type:'decrement'})}>decrement by 10</button>
 //         </>
 //     )
-// }
-import { useReducer } from "react"
-function valueChanger(state,action){
-    switch(action.type){
-     case'increment':{
-       return{
-        value:state.value+10
-     };
-     }
-     case'decrement':{
-        return{
-        value:state.value-10
-        };
-     }
-     default :{
-        return
-            state
-        
-     }
-    }
-}
-export default function Reducers(){
-    const [state,dispatch]=useReducer(valueChanger,{value:10});
-return(
-    <>
-    
-    <h1>{state.value}</h1>
-    <button onClick={()=>dispatch({type:'increment'})}>increse by <b>10</b></button>
-    <button onClick={()=>dispatch({type:'decrement'})}>decrese by <b>10</b></button>
-    </>
-)
-}
+// } 
 
-//hear we define the reducer with state,and dispatch the initial value of state is 10 and second thing was dispatch function to updated value state and also we make or call valuechanger reducer function in gui portion we make two button and apply click event call the dispatch function and pass the event name like increse or decrese the reducer function take the value from dispatch function and initial state check in switch case and return according value
+/// question) 1 
+// import React, { useReducer } from 'react';
+
+// const initialState = { isVisible: true };
+
+// function reducer(state, action) {
+//     switch (action.type) {
+//         case 'toggle':
+//             return { isVisible: !state.isVisible };
+//         default:
+//             return state;
+//     }
+// }
+
+// export default function Reducers() {
+//     const [state, dispatch] = useReducer(reducer, initialState);
+//     return (
+//         <div>
+//             {state.isVisible && <p>This is a visible text</p>}
+//             <button onClick={() => dispatch({ type: 'toggle' })}>
+//                 {state.isVisible ? 'Hide' : 'Show'}
+//             </button>
+//         </div>
+//     );
+// }
+
+//question2: input field and age manage both 
+
+import React, { useReducer } from "react";
+
+export default function Reducers() {
+    const initialState = { name: 'hulk', age: 20 };
+
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'getName':
+                return {
+                    ...state,
+                    name: action.nextName
+                };
+            case 'incrementAge':
+                return {
+                    ...state,
+                    age: state.age + 1
+                };
+            default:
+                return state;
+        }
+    }
+
+    function updateName(e) {
+        dispatch({
+            type: 'getName',
+            nextName: e.target.value
+        });
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <>
+            <input onChange={updateName} />
+            <button onClick={() => dispatch({ type: 'incrementAge' })}>Increment Age</button>
+            <h1>Hello {state.name}, you are {state.age} years old.</h1>
+        </>
+    );
+}
